468895beaf2f977923396b1e97e50f056f7f05ff
Rob Spectre <rob@brooklynhacker.com>
2022-03-19 17:48:52 -0400

    Added slashes to base directive in vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "f1bingo.com",
+  base: "/f1bingo.com/",
   resolve: {
     alias: [ 
       { find: '@', replacement: '/src' }
