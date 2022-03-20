ca9fe87e470153f13980325af507171bed1153f5
Rob Spectre <rob@brooklynhacker.com>
2022-03-19 22:21:06 -0400

    Eliminate base dir from vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/f1bingo.com/',
   resolve: {
     alias: [ 
       { find: '@', replacement: '/src' }
